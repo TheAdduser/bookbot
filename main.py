0ef8f55bc7eb4ffdc532e2a59d72503c3f7bed22
Bartosz Wrona <EasyRider190@gmail.com>
2025-03-30 14:49:35 +0200

    create sort function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_character_count
+filepath = 'books/frankenstein.txt'
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,9 +7,16 @@ def get_book_text(path_to_file):
         return file_contents
         
 def main():
-    book_text = (get_book_text('books/frankenstein.txt'))
-    print(f"{count_words(book_text)} words found in the document")
-    print(f"{count_characters(book_text)} characters found in the document")
-    
+    book_text = (get_book_text(filepath))
+
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print(f"----------- Word Count ----------")
+    print(f"Found {count_words(book_text)} total words")
+    print(f"--------- Character Count -------")
+    print(sort_character_count(book_text))
+    # for letter in sort_character_count(book_text):
+    #     if letter.isalpha(letter) == True:
+    #         print(f"{letter["name"]}: {letter["count"]}")
     
 main()
--- stats.py
+++ stats.py
@@ -4,7 +4,8 @@ def count_words(text):
     return len(split_text)
         
 def count_characters(text):
-    letter_count = {}
+    letter_count = {
+    }
     text = text.lower()
     
     for letter in text:
@@ -13,4 +14,17 @@ def count_characters(text):
         else:
             letter_count[letter] = 1
 
+    return letter_count
+
+def sort_character_count(letter_count):
+    list_of_dicts = []
+    
+    def sort_on(dict):
+        return dict["count"]
+    
+    for character in letter_count:
+        d = { "name" : character, "count" : letter_count[character]}
+        list_of_dicts.append(d)
+    
+    list_of_dicts.sort(reverse=True, key = sort_on)
     return letter_count
